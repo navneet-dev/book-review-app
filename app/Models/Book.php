43fd1f93910710ff643159d8f93004d6b48e85cb
Navneet <navneetkmr029@gmail.com>
2026-02-02 20:28:24 +0530

    book model changes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -11,5 +13,30 @@ class Book extends Model
     use HasFactory;
     public function reviews(){
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title) : Builder | QueryBuilder
+    {
+        return $query->where('title', 'like', '%'. $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from=null, $to=null) : Builder | QueryBuilder
+    {
+        return $query->withCount(['reviews' => function(Builder $q) use ($from, $to){
+            if($from && !$to){
+                $q->where('created_at', '>=', $from);
+            }
+            elseif(!$from && $to){
+                $q->where('created_at', '<=', $to);
+            }
+            elseif($from && $to){
+                $q->whereBetween('created_at', [$from, $to]);
+            }
+        }])->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder | QueryBuilder
+    {
+        return $query->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating', 'desc');
     }
 }
