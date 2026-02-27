b481fa889808c90f340308ad30950d57f0f07148
Navneet <navneetkmr029@gmail.com>
2026-02-27 13:16:04 +0530

    rate limit changed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
         // Define a limiter for 
         RateLimiter::for('reviews', function (Request $request) {
             // Allows 2 requests per minute based on IP address
-            return Limit::perHour(2)->by($request->ip())->response(function (Request $request, array $headers) {
+            return Limit::perHour(3)->by($request->ip())->response(function (Request $request, array $headers) {
                 return response('You can store only 2 review per hour!', 429);
             });
         });
